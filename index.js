a359aa17a94f5be119f47a699bdc1bf0d3b74658
Saksham329 <Shivamssharma398@gmail.com>
2024-10-13 03:06:33 +0530

    complete website

---
+++ index.js
@@ -0,0 +1,134 @@
+import express from "express";
+import pg from "pg";
+import bodyParser from "body-parser";
+import path from "path"
+import { fileURLToPath } from "url"
+import dotenv from "dotenv";
+
+
+dotenv.config();
+const app = express()
+const port = process.env.port
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("view engine","ejs")
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended:false}));
+
+const db = new pg.Client({
+    user: process.env.db_user,
+    host:process.env.db_host,
+    database:process.env.db_database,
+    password:process.env.db_password,
+    port:process.env.db_port,
+})
+db.connect()
+
+app.get("/",async(req,res)=>{
+    try {
+        const books = await db.query(`SELECT * FROM books `)
+        const results = books.rows
+        res.render("index.ejs", { books: results });
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.get("/add",async(req,res)=>{
+    res.render("addPost")
+})
+
+app.post("/add",async(req,res)=>{
+    try {await db.query(`INSERT INTO books(title,isbn,summary,rating) VALUES ($1,$2,$3,$4)`,
+        [req.body.title,req.body.isbn,req.body.summary,req.body.rating]);
+
+        res.redirect("/")
+        
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.post("/edit",async (req,res)=>{
+   
+    try { const book_Id=req.body.book_id
+        console.log(book_Id)
+        
+        const books = await db.query(`SELECT * FROM books WHERE id = $1`,[book_Id])
+        const results = books.rows[0]
+        console.log(results.title)
+        res.render("editPost", { book: results })
+    } catch (error) {
+        console.log(error)
+    }
+    
+});
+
+app.post("/save",async(req,res)=>{
+    const { upd_title , upd_isbn, upd_summary, upd_rating, upd_book_id} = req.body;
+    try {
+        await db.query(`UPDATE books SET title = $1,  isbn = $2 ,  summary = $3 , rating = $4 WHERE id = $5 `,[
+        upd_title, upd_isbn, upd_summary, upd_rating, upd_book_id]
+        );
+        res.redirect("/")
+    } catch (error) {
+        console.log(error)
+    }
+   
+})
+
+app.post("/delete",async(req,res)=>{
+    const book_id=req.body.upd_book_id
+    
+    try {
+        await db.query(`DELETE FROM reviews WHERE review_id = $1`, [book_id]);
+        await db.query(`DELETE FROM books WHERE id = $1`,[book_id])
+    } catch (error) {
+        console.log(error)
+    }
+    res.redirect("/")
+})
+
+app.post("/reviews",async (req,res)=>{
+    try { const book_Id=req.body.book_id
+        console.log(book_Id)
+        const books = await db.query(`select * from books WHERE books.id = $1`,[book_Id])
+        const results = books.rows[0]
+        const reviews = await db.query(`select * from reviews WHERE review_id=$1`,[book_Id])
+        const results2 = reviews.rows
+        res.render("reviews", { book: results,reviews:results2 })
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+app.get("/reviews",async (req,res)=>{
+    try { const book_Id=req.body.book_id
+        console.log(book_Id)
+        const books = await db.query(`select * from books WHERE books.id = $1`,[book_Id])
+        const results = books.rows[0]
+        const reviews = await db.query(`select * from reviews WHERE review_id=$1`,[book_Id])
+        const results2 = reviews.rows
+        res.render("reviews", { book: results,reviews:results2 })
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+app.post("/addReview",async (req,res)=>{
+    try {const review_Id=req.body.review_id;
+        const book_Id = req.body.review_id
+        const review=req.body.review
+        
+        await db.query(`INSERT INTO reviews(review_id,review) VALUES($1,$2)`,[review_Id,review])
+        res.redirect(`/`)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.listen(port,()=>{
+    console.log(`port is running on ${port}`)
+})
